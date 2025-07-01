867271993b053c9df6c8fef51be2ede9c68dd63b
Tomasz Kosinski <tomasz.kosinski@tatukgis.com>
2025-07-01 09:31:09 +0200

    v103

---
+++ MAUI/ViewerLite/MauiProgram.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.Extensions.Logging;
+using SkiaSharp.Views.Maui.Controls.Hosting;
+using TatukGIS.RTL;
+
+namespace ViewerLite
+{
+    public static class MauiProgram
+    {
+        public static MauiApp CreateMauiApp()
+        {
+            var builder = MauiApp.CreateBuilder();
+            builder
+                .UseMauiApp<App>()
+                .UseSkiaSharp()
+                .ConfigureFonts(fonts =>
+                {
+                    TatukGIS.RTL.FontList.Load(fonts);
+                });
+
+#if DEBUG
+    		builder.Logging.AddDebug();
+#endif
+
+            return builder.Build();
+        }
+    }
+}
